0b8df673036ba853204ab864e04fd1072527c1fb
Andrés Chávez <andres@deadline.com.mx>
2024-10-07 16:56:15 -0600

    added: responsive contact page

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,13 +1,30 @@
 import CustomHeader from "../components/header/custom-header";
-import styles from "../page.module.css";
+import styles from "./page.module.css";
+import contactCover from "../../public/images/contact-cover.png";
+import CustomFooter from "../components/footer/custom-footer";
 
 export default function Home() {
   return (
     <>
       <CustomHeader />
       <main className={styles.mainWrapper}>
-        <p>Página de Contacto</p>
+        <div className={styles.redWrapper}>
+          <img src={contactCover.src} alt="Contacto" />
+        </div>
+        <div className={styles.bottomWrapper}>
+          <form className={styles.formWrapper}>
+            <h6>Contacto</h6>
+            <label htmlFor="name">Nombre:</label>
+            <input id="name" type="text" name="name" placeholder="" />
+            <label htmlFor="email">Correo electrónico:</label>
+            <input id="email" type="email" name="email" placeholder="" />
+            <label htmlFor="message">Escribe tu mensaje:</label>
+            <textarea id="message" name="message" placeholder=""></textarea>
+            <button>Enviar</button>
+          </form>
+        </div>
       </main>
+      <CustomFooter />
     </>
   );
 }
