4381c9b2920b86251de0ecb56ca275c4bf2593d3
Andrés Chávez <andres@deadline.com.mx>
2024-10-11 14:20:32 -0600

    added: details blog page with responsive

---
+++ app/blog/[slug]/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../../globals.css";
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Parma | Blog Details",
+  description: "Marca premium de Sabori",
+};
+
+//Add google fonts gotham
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
